1ecebe8ab71e2d9c6185b4d93445a2af8509c029
korolchuk111 <anna.korolchuk@oa.edu.ua>
2022-06-15 14:26:20 +0300

    All pages

--- BlazorWebApp/Program.cs
+++ BlazorWebApp/Program.cs
@@ -1,3 +1,4 @@
+
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -20,6 +21,8 @@ namespace BlazorWebApp
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
+
+         
         }
     }
 }
