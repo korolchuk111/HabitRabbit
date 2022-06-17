9468fcde3eea6bded8dc6a79ff01255ac7872912
korolchuk111 <anna.korolchuk@oa.edu.ua>
2022-06-17 17:21:39 +0300

    small fixes

--- Shared/Shared/DailyTaskDTO/DailyTaskDTO.cs
+++ Shared/Shared/DailyTaskDTO/DailyTaskDTO.cs
@@ -1,16 +1,13 @@
-using System;
+namespace Shared.DailyTaskDTO;
 
-namespace Shared.DailyTaskDTO
+public class DailyTaskDTO
 {
-    public class DailyTaskDTO
-    {
-        public int Id { get; set; }
-        public string ChallengeTitle { get; set; }
-        public int CountOfUnits { get; set; }
-        public string UnitShortName { get; set; }
-        public DateTime Date { get; set; }
-        public int CountOfUnitsDone { get; set; }
-        public int PercentOfDone { get; set; }
-        public bool IsDone { get; set; }
-    }
+    public int Id { get; set; }
+    public string ChallengeTitle { get; set; }
+    public int CountOfUnits { get; set; }
+    public string UnitShortName { get; set; }
+    public DateTime Date { get; set; }
+    public int CountOfUnitsDone { get; set; }
+    public int PercentOfDone { get; set; }
+    public bool IsDone { get; set; }
 }
