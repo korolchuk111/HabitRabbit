ad9be2d4c0b5d1515e09b5dc5547f86823abb936
tonia4ik-s <cooltone4ka@gmail.com>
2022-06-17 22:06:59 +0300

    fix dtos

--- Shared/Shared/DailyTaskDTO/DailyTaskDTO.cs
+++ Shared/Shared/DailyTaskDTO/DailyTaskDTO.cs
@@ -1,13 +1,16 @@
-namespace Shared.DailyTaskDTO;
+using System;
 
-public class DailyTaskDTO
+namespace Shared.DailyTaskDTO
 {
-    public int Id { get; set; }
-    public string ChallengeTitle { get; set; }
-    public int CountOfUnits { get; set; }
-    public string UnitShortName { get; set; }
-    public DateTime Date { get; set; }
-    public int CountOfUnitsDone { get; set; }
-    public int PercentOfDone { get; set; }
-    public bool IsDone { get; set; }
+    public class DailyTaskDTO
+    {
+        public int Id { get; set; }
+        public string ChallengeTitle { get; set; }
+        public int CountOfUnits { get; set; }
+        public string UnitShortName { get; set; }
+        public DateTime Date { get; set; }
+        public int CountOfUnitsDone { get; set; }
+        public int PercentOfDone { get; set; }
+        public bool IsDone { get; set; }
+    }    
 }
