24ffed597125b84549e0370b499d4978cdcd220b
tonia4ik-s <cooltone4ka@gmail.com>
2022-06-19 20:26:59 +0300

    last fixes for progress

--- Shared/Shared/UserDTO/UpdateUserDTO.cs
+++ Shared/Shared/UserDTO/UpdateUserDTO.cs
@@ -1,8 +1,9 @@
-namespace Shared.UserDTO;
-
-public class UpdateUserDTO
-{ 
-    public string UserName { get; set; }
-    public string NewUserName { get; set; }
-    public string NewEmail { get; set; }
+namespace Shared.UserDTO
+{
+    public class UpdateUserDTO
+    { 
+        public string UserName { get; set; }
+        public string NewUserName { get; set; }
+        public string NewEmail { get; set; }
+    }    
 }
